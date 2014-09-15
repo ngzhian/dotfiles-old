3912f50d8602db6f8543a34c9a05ec1f394c17a2
Ng Zhi An <ngzhian@gmail.com>
2014-09-14 23:30:52 -0400

    update scripts

--- bin/push.sh
+++ bin/push.sh
@@ -1,2 +1,25 @@
-cp -v -r ~/dotfiles/{bin,.vim,.xmonad} ~/.
-cp -v ~/dotfiles/{.bash_aliases,.bashrc,.gitconfig,.vimrc,.xinitrc,.xmobarrc,.Xresources} ~/.
+#!/usr/bin/bash
+
+# bash
+cp -v ~/dotfiles/.bash_aliases ~/.bash_aliases
+cp -v ~/dotfiles/.bashrc ~/.bashrc
+
+
+# bin
+cp -v -r ~/dotfiles/bin ~/.
+
+# vim
+cp -v ~/dotfiles/.vimrc ~/.
+cp -rv ~/dotfiles/.vim/colors ~/.vim/colors
+
+# git
+cp -v ~/dotfiles/.gitconfig ~/.
+
+# x
+cp -v ~/dotfiles/.xinitrc ~/.
+cp -v ~/dotfiles/.Xresources ~/.
+
+# tmux
+cp -v ~/.tmux.conf ~/dotfiles/.
+cp -v ~/dotfiles/.tmux.conf ~/.tmux.conf
+
